04f91ebf8ac9e720c69b46ba800df99bf0a7b109
Prakharnamdeo15 <114492792+Prakharnamdeo15@users.noreply.github.com>
2024-10-06 18:46:39 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import Image from "next/image";
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
 
 import KeyBpm from "./keyBpm/page";
 import Cutter from "./cutter/page"
